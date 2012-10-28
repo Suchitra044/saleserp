ea7648604b9e0d5961fc04c846a8fc8c90b13b04
Suchitra044 <suchitrajambugeswaran@gmail.com>
2012-10-29 00:08:03 +0530

    Update cart.php

--- cart.php
+++ cart.php
@@ -1,67 +1,12 @@
 <?php
 // Include MySQL class
-//require_once('mysql.class.php');
+require_once('mysql.class.php');
 
 // Include database connection
-//require_once('global.inc.php');
+require_once('global.inc.php');
 // Include functions
-//require_once('functions.inc.php');
+require_once('functions.inc.php');
 
-$host = 'a.db.shared.orchestra.io';
-$user = 'user_d0d277c5';
-$pass = 'V$!Q7Z$F0J1h^d';
-$name = 'db_d0d277c5';
-$db = &new MySQL($host,$user,$pass,$name);
-
-function writeShoppingCart() {
-	$cart = $_SESSION['cart'];
-	if (!$cart) {
-		return '<p>You have no items in your shopping cart</p>';
-	} else {
-		// Parse the cart session variable
-		$items = explode(',',$cart);
-		$s = (count($items) > 1) ? 's':'';
-		return '<p>You have <a href="cart.php">'.count($items).' item'.$s.' in your shopping cart</a></p>';
-	}
-}
-
-function showCart() {
-	global $db;
-$cart = $_SESSION['cart'];
-	if ($cart) {
-		$items = explode(',',$cart);
-		$contents = array();
-		foreach ($items as $item) {
-			$contents[$item] = (isset($contents[$item])) ? $contents[$item] + 1 : 1;
-		}
-		$output[] = '<form action="cart.php?action=update" method="post" id="cart">';
-		$output[] = '<table>';
-		foreach ($contents as $id=>$qty) {
-			$sql = 'SELECT * FROM Product WHERE pid = '.$id;
-			$result = $db->query($sql);
-//$result=mysql_query($query, $c);
-			$row = $result->fetch();
-			extract($row);
-			$output[] = '<tr>';
-			$output[] = '<td><a href="cart.php?action=delete&id='.$id.'" class="r">Remove</a></td>';
-			$output[] = '<td>'.$detail.' and '.$pid.'</td>';
-			$output[] = '<td>&pound;'.$price.'</td>';
-			$output[] = '<td><input type="text" name="qty'.$id.'" value="'.$qty.'" size="3" maxlength="3" /></td>';
-			$output[] = '<td>&pound;'.($price * $qty).'</td>';
-			$total += $price * $qty;
-			$output[] = '</tr>';
-		}
-		$output[] = '</table>';
-		$output[] = '<p>Grand total: <strong>&pound;'.$total.'</strong></p>';
-		$output[] = '<div><button type="submit">Update cart</button></div>';
-		$output[] = '</form>';
-	} else {
-		$output[] = '<p>You shopping cart is empty.</p>';
-	}
-	return join('',$output);
-}
-
-?>
 // Start the session
 session_start();
 // Process actions
