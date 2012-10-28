ffa8e3d7ba5d6eacca71773097124157293b98e6
Suchitra044 <suchitrajambugeswaran@gmail.com>
2012-10-28 23:18:40 +0530

    Update cart.php

--- cart.php
+++ cart.php
@@ -1,12 +1,6 @@
-
 <?php
 // Include MySQL class
-
-$host = 'a.db.shared.orchestra.io';
-$user = 'user_d0d277c5';
-$pass = 'V$!Q7Z$F0J1h^d';
-$name = 'db_d0d277c5';
-$db = &new MySQL($host,$user,$pass,$name);
+require_once('mysql.class.php');
 
 // Include database connection
 require_once('global.inc.php');
