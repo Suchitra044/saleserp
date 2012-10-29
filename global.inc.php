c632816b1e76b8135aeae065d9feb18141a3621a
Suchitra044 <suchitrajambugeswaran@gmail.com>
2012-10-29 11:34:05 +0530

    Update global.inc.php

--- global.inc.php
+++ global.inc.php
@@ -3,5 +3,6 @@ $host = 'a.db.shared.orchestra.io';
 $user = 'user_d0d277c5';
 $pass = 'V$!Q7Z$F0J1h^d';
 $name = 'db_d0d277c5';
-$db = &new MySQL1($host,$user,$pass,$name);
+$db = &new MySQL($host,$user,$pass,$name);
+MySQL();
 ?>
